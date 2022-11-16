53fea5b785f6e5e8a4c25e17f6fb820f356cd78b
hftamayo <hftamayo@gmail.com>
2022-11-16 15:44:15 -0600

    adjusting save method for messages

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
@@ -20,24 +21,29 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: "Validation failed, entered data is incorrect",
-        errors: errors.array(),
-      });
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect",
+      errors: errors.array(),
+    });
   }
   const title = req.body.title;
   const content = req.body.content;
-  //create the post in the db
-  res.status(201).json({
-    message: "Post created successfully",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: { name: "hftamayo" },
-      createdAt: new Date(),
-    },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/grub03.jpg",
+    creator: { name: "hftamayo" },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
