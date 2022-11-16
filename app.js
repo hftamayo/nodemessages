37e52a7aec74563678a5df1aee3ee619e8333a25
hftamayo <hftamayo@gmail.com>
2022-11-16 14:22:54 -0600

    connecting to mongodb

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const feedRoutes = require('./routes/feed');
+const feedRoutes = require("./routes/feed");
 
 const app = express();
 
@@ -9,14 +10,22 @@ app.use(bodyParser.json());
 
 //CORS handler
 app.use((req, res, next) => {
-    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9090', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Origin', '*');    
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9090', 'http://localhost:3000');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
 
-app.listen(3006);
+mongoose
+  .connect("mongodb+srv://node:Node123$@nodecluster.hfnls.mongodb.net/shop")
+  .then(result => {
+      app.listen(3006);
+  })
+  .catch(err => console.log(err));
 
