3a2849d3ff7e2860c743833a9b4b190383a05fb3
hftamayo <hftamayo@gmail.com>
2022-11-17 14:42:36 -0600

    handling static files for uploading part1

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 //CORS handler
 app.use((req, res, next) => {
@@ -21,10 +23,16 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
 mongoose
   .connect("mongodb+srv://changeme:changeme$@nodecluster.hfnls.mongodb.net/messages")
-  .then(result => {
-      app.listen(3006);
+  .then((result) => {
+    app.listen(3006);
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
