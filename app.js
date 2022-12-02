0ad1324e9f0913cfd2aa4065d196754932425efa
hftamayo <hftamayo@gmail.com>
2022-12-02 14:59:22 -0600

    working on cors for PUT and DELETE

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const path = require("path");
 const multer = require("multer");
+const cors = require("cors");
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
+
+const allowedOrigins = ['http://localhost:3000',
+                         'http://127.0.0.1:3000'];
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -42,20 +46,33 @@ app.use(
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 //CORS handler
-app.use((req, res, next) => {
+app.use(
+  cors({
+    origin: function(origin, callback){
+      //allow requests with no origin like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        const msg = 'This site does not allow access from the specified origin';
+        return callback(new Error(msg), false);
+      }
+    },
+    allowedHeaders: ["authorization", "Content-Type"],
+    exposedHeaders: ["authorizartion"],
+    methods: "GET,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+  }),
+);
+
+//app.use((req, res, next) => {
   //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9090', 'http://localhost:3000');
-  res.setHeader("Access-Control-Allow-Origin", "*");
+/*   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-/*   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  ); */
   next();
-});
+}); */
 
 app.use("/feed", feedRoutes);
 app.use((error, req, res, next) => {
