4dcbec4b18915f2886013d3f6d7e616a6d804f09
hftamayo <hftamayo@gmail.com>
2023-01-13 09:35:58 -0600

    generate a jwt part2

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,25 +42,30 @@ exports.login = (req, res, next) => {
   const password = req.body.password;
   let loadedUser;
   User.findOne({ email: email })
-    .then(user => {
-      if(!user){
-        const error = new Error('Email or password not found');
+    .then((user) => {
+      if (!user) {
+        const error = new Error("Email or password not found");
         error.statusCode = 401;
         throw error;
       }
       loadedUser = user;
       return bcrypt.compare(password, user.password);
     })
-    .then(isEqual => {
-      if(!isEqual){
-        const error = new Error('Email or password not found, please check');
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error("Email or password not found, please check");
         error.statusCode = 401;
         throw error;
       }
-      const token = jwt.sign({
-        email: loadedUser.email,
-        userId: loadedUser._id.toString()
-      });
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        "somesupersecretstuff",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({token: token, userId: loadedUser._id.toString()});
     })
     .catch((err) => {
       if (!err.statusCode) {
