9fcac8a6575bb2eda8e500a5af91cd5fb042aa1b
hftamayo <hftamayo@gmail.com>
2023-02-09 10:22:12 -0600

    setting up the server listening socket

--- app.js
+++ app.js
@@ -103,6 +103,10 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(GATEWAY)
   .then((result) => {
-    app.listen(3006);
+    const server = app.listen(3006);
+    const io = require('socket.io')(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    })
   })
   .catch((err) => console.log(err));
